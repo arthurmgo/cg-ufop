2b07a111e0a813212071689f6fceb5b6e63f3359
Mariane Vieira <mafonsovieir2015@my.fit.edu>
2017-07-23 01:55:52 -0400

    Iluminacao

--- main.cpp
+++ main.cpp
@@ -80,6 +80,7 @@ void Canon()
 
 }
 
+
 void DivisaoCampo()
 {
     glPushMatrix();
@@ -321,21 +322,32 @@ void Alvo()
 {
     glPushMatrix();
 
-
-    //random_variable = rand();
-    //double d_x = 0.0;
-
-
     glTranslated(d_x, 0.0, -d_z);
 
     glColor3f(0.0, 1.0, 1.0);
 
     glScaled(2.0, 4.0, 2.0);
 
-    glutWireCube(1);
+    glutSolidCube(1);
 
     glPopMatrix();
 
+}
+
+void MarcadorForca()
+{
+    glPushMatrix();
+
+    glTranslated(5.0, 5.0, 0.0);
+    glColor3f(1.0, 1.0, 0.0);
+    glBegin(GL_QUADS);
+        glVertex3f(0.0,10.0,0.0);
+        glVertex3f(2.0,12.0,0.0);
+        glVertex3f(0.0,10.0,0.0);
+        glVertex3f(0.0,12.0,0.0);
+    glEnd();
+
+    glPopMatrix();
 }
 
 void TerrenoBase()
@@ -346,15 +358,14 @@ void TerrenoBase()
 
     Alvo();
 
+    MarcadorForca();
+
     glColor3f(1.0, 0.0,0.0);
     glScaled(1.0, 4.0, 1.0);
     glutWireCube(1);
 
     glPopMatrix();
 }
-
-
-
 
 
 void Desenha(void)
@@ -394,8 +405,6 @@ void Desenha(void)
 }
 
 
-
-
 void AlteraTamanhoJanela (int w, int h)
 {
     glViewport (0, 0, (GLsizei) w, (GLsizei) h);
@@ -409,6 +418,7 @@ void AlteraTamanhoJanela (int w, int h)
     glLoadIdentity();
     gluLookAt(0,1,13, 0,4,0, 0,1,0); // Especifica posição do observador e do alvo
 }
+
 
 void Teclado(unsigned char key, int x, int y)
 {
@@ -463,6 +473,43 @@ void Teclado(unsigned char key, int x, int y)
 
 }
 
+
+void ativaIluminacao (void){
+	GLfloat luzAmbiente[4]={0.2,0.2,0.2,1.0};
+	GLfloat luzDifusa[4]={0.7,0.7,0.7,1.0};	   // "cor"
+	GLfloat luzEspecular[4]={1.0, 1.0, 1.0, 1.0};// "brilho"
+	GLfloat posicaoLuz[4]={0.0, 50.0, 50.0, 1.0};
+	// Capacidade de brilho do material
+	GLfloat especularidade[4]={1.0,1.0,1.0,1.0};
+	GLint especMaterial = 60;
+	// Define a refletância do material
+	glMaterialfv(GL_FRONT,GL_SPECULAR, especularidade);
+	// Define a concentração do brilho
+	glMateriali(GL_FRONT,GL_SHININESS,especMaterial);
+
+	// Ativa o uso da luz ambiente
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, luzAmbiente);
+
+	// Define os parâmetros da luz de número 0
+	glLightfv(GL_LIGHT0, GL_AMBIENT, luzAmbiente);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, luzDifusa );
+	glLightfv(GL_LIGHT0, GL_SPECULAR, luzEspecular );
+	glLightfv(GL_LIGHT0, GL_POSITION, posicaoLuz );
+
+    // Habilita a definição da cor do material a partir da cor corrente
+	glEnable(GL_COLOR_MATERIAL);
+	//Habilita o uso de iluminação
+	glEnable(GL_LIGHTING);
+	// Habilita a luz de número 0
+	glEnable(GL_LIGHT0);
+	// Habilita o depth-buffering
+	glEnable(GL_DEPTH_TEST);
+
+	// Habilita o modelo de colorização de Gouraud
+	glShadeModel(GL_SMOOTH);
+}
+
+
 void Timer(int value)
 {
     if(tiro.vis){
@@ -496,6 +543,8 @@ int main(int argc, char** argv)
     glutTimerFunc(10,Timer,0);
 
     glutDisplayFunc(Desenha);
+
+    ativaIluminacao();
 
     glutReshapeFunc(AlteraTamanhoJanela);
 

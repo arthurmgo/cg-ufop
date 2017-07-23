d67937bdb9e7df640cfe654a4c2f284555ff4c68
Arthur Miranda Gomes <arthur-m-gomes@hotmail.com>
2017-07-23 01:28:29 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -369,7 +369,7 @@ void AlteraTamanhoJanela (int w, int h)
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(0,1,13, 0,4,0, 0,1,0); // Especifica posição do observador e do alvo
+    gluLookAt(0,1,13, 0,4,0, 0,1,0); // Especifica posiÃ§Ã£o do observador e do alvo
 }
 
 void Teclado(unsigned char key, int x, int y)
@@ -416,7 +416,7 @@ void Teclado(unsigned char key, int x, int y)
 
 }
 
-void Timer(int value)//teste
+void Timer(int value)
 {
     if(tiro.vis){
          if(tiro.y > 0.2){
